bc036ee15f3604d441f96fc9cf459e0e94792f74
xjq <lebnizjoan@gmail.com>
2024-06-22 02:23:32 +0800

    docs(example): 注释补充

--- packages/example/user-info/index.tsx
+++ packages/example/user-info/index.tsx
@@ -3,6 +3,12 @@ import UserCard from './user-card';
 import S from './index.module.less';
 import UserList from './user-list';
 
+/**
+ * 用户信息 Demo 页
+ *
+ * @export
+ * @return {*}
+ */
 export default function Component() {
   return (
     <div className={S.container}>
